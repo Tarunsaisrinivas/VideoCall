44a9737c4acd11adf2807441ce34ef90cc44ab3e
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-05-06 13:41:37 +0530

    updated

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/login',Login);
-router.post('/signup',Signup);
-router.post('/logout',isLogin,Logout);
+// router.post('/login',Login);
+// router.post('/signup',Signup);
+// router.post('/logout',isLogin,Logout);
 
 export default router;
